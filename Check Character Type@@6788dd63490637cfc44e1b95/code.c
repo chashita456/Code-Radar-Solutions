06a7ac170d1622dde093934bee2fc148238b9d9b
chashita456 <chashita1019.becse24@chitkara.edu.in>
2025-01-28 17:37:58 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -3,16 +3,16 @@ int main(){
     char ch;
     scanf("%c",&ch);
     if(ch=='A'||ch=='E'|| ch=='I'||ch=='0'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='0'||ch=='u'){
-        printf("vowel");
+        printf("Vowel");
     }
     else if(ch=='B'||ch=='C'||ch=='D'||ch=='F'|| ch=='G'||ch=='H'||ch=='J'||ch=='K'||ch=='L'||ch=='M'||ch=='N'||ch=='P'||ch=='Q'||ch=='R'||ch=='S'||ch=='T'||ch=='V'||ch=='W'||ch=='X'||ch=='Y'||ch=='Z'||ch=='c'||ch=='b'||ch=='d'||ch=='f'ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='o'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z'){
-        printf("consonant");
+        printf("Consonant");
     }
     else if(ch>='0' && ch<='9'){
         printf("Digit");
     }
     else{
-        printf(" special character");
+        printf(" Special Character");
     }
     return 0;
 }
