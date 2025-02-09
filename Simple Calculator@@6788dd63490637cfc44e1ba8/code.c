d582d4f63f49d6991fbab53fdb111145957c74d1
chashita456 <chashita1019.becse24@chitkara.edu.in>
2025-02-09 17:19:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,13 +9,13 @@ switch(operator){
     case '+':
     printf("%d",num1+num2);
     break;
-    case'-':
+    case '-':
     printf("%d",num1-num2);
     break;
-    case'*':
+    case '*':
     printf("%d",num1*num2);
     break;
-    case"/":
+    case "/":
     if(num2!=0){
         printf("%d",num1/num2);
     }
