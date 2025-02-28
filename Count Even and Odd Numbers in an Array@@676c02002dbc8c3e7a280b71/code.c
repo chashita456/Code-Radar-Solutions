863e5094958b3d7217cc5f8f2524dafa1aa0ddad
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-02-28 13:15:55 +0530

    Solution to Count Even and Odd Numbers in an Array

--- Count Even and Odd Numbers in an Array@@676c02002dbc8c3e7a280b71/code.c
+++ Count Even and Odd Numbers in an Array@@676c02002dbc8c3e7a280b71/code.c
@@ -1,30 +1,36 @@
-#include<stdio.h>
-int countodd(int arr[n],int a);
-int counteven(int arr[n],int a);
-int countodd(int arr[n],int a){
-    for(int i=0;i<=a;i++){
-        int count =0;
-        if(arr[i]%2!=0){
-            count =count +1;
-        }
-    }
-}
-int counteven(int arr[n],int a){
-    for(int i=0;i<=a;i++){
-        int count =0;
-        if(arr[i]%2==0){
-            count =count +1;
-        }
-    }
-    int main(){
-        int a;
-        scanf("%d",&a);
-        int arr[n];
-        scanf("%d",&arr[n]);
-        printf("%d",countodd (arr,a));
-        printf("%d",counteven (arr,a));
-        return 0;
-        
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Ask the user for the size of the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    // Declare the array of size 'n'
+    int arr[n];
+
+    // Ask the user to input the elements of the array
+    printf("Enter %d integers: \n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // Read each element into the array
     }
 
+    int evenCount = 0, oddCount = 0;
+
+    // Loop through the array and count even and odd numbers
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            evenCount++;  // Increment even count if the number is even
+        } else {
+            oddCount++;   // Increment odd count if the number is odd
+        }
+    }
+
+    // Print the results
+    printf("Number of even numbers: %d\n", evenCount);
+    printf("Number of odd numbers: %d\n", oddCount);
+
+    return 0;
 }
+
