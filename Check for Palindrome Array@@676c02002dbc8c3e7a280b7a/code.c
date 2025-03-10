cf2435cd4076ec2649cef35f7668826b0b923bae
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-03-10 16:58:31 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,37 +1,28 @@
 #include<stdio.h>
-int rev(int arr[n],int a);
-int palidrome(int arr[n],int a);
-int rev(int arr[],int a){
-    for(int i=0;i<=a/2;i++){
-        int first = arr[i];
-        int second =arr[a-i-1];
-        arr[i] = second;
-        arr[a-i-1] = first;
-        
-    }
-    for(int i=0;i<=a;i++){
-        printf("%d\t",arr[i]);
-    }
-    printf("\n");
-}
-int palidrome(int arr[],int a){
-    for(int i=0;i<a/2;i++){
-        if(arr[i]==arr[a-i-1]){
-            printf("palidrome");
-        }
-        else{
-            printf("not palidrome");
-        }
-    }
-}
 int main(){
     int a;
     scanf("%d",&a);
-    int arr[n];
-    for(int i=0;i<=10;i++){
+    int arr[a];
+    for(int i=0;i<=a;i++){
         scanf("%d",&arr[i]);
     }
-    return 0;
+    int brr[a];
+    for(int i=0;i<=a;i++){
+        brr[i] =arr[a-i-1];
+    }
+    int palidrom=1;
+    for(int i=0;i<=a;i++){
+        brr[i]!=arr[i];
+        palidrom =0;
+        break;
+    }
+    if(palidrome){
+        printf("%d",YES);
+    }
+    else{
+        printf("%d",NO)
+    }
+    return o;
 }
 
     
