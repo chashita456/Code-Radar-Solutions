88026f6761c1223a2222f2cf79c0c8bb003223ad
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-03-11 16:38:16 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,39 +1,25 @@
-#include <stdio.h>
-
-int main() {
-    int n;
-
-    // Read the size of the array
-    
-    scanf("%d", &n);
-
-    int arr[n];  // Declare an array of size n
-
-    // Read array elements from the user
-    
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[a]);
     }
-
-    // To track which elements we've already printed
-    int printed[n];  // Array to track printed elements, initialized to 0
-    for (int i = 0; i < n; i++) {
-        printed[i] = 0; // Initialize all elements of printed array to 0
+    int printed[a];
+    for(int i=0;i<a;i++){
+        printed[i]=0;
     }
-
-    // Iterate through the array to find frequency of each element
-    for (int i = 0; i < n; i++) {
-        if (printed[i] == 0) { // If the element hasn't been printed yet
-            int count = 1;  // Start count from 1 for the current element
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] == arr[j]) {  // If arr[j] is the same as arr[i]
-                    count++;
-                    printed[j] = 1;  // Mark arr[j] as visited
-                }
-            }
-            printf("%d %d\n", arr[i], count);  // Print frequency of arr[i]
+    for(int i=0;i<a;i++){
+        if(printed[i]==0){
+            int count =1;
+        }
+        for(int j=0;j<a;j++){
+            if (arr[i] == arr[j])
+            count++
+            printed[j]=1;
         }
     }
-
+    printf("%d %d",arr[i],count);
     return 0;
 }
