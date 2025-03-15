b8d13e5aed5c3ed5e94f27a448f863caf7e7ef6a
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-03-15 11:24:20 +0530

    Solution to Rotate an Array to the Right by K Steps

--- Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
+++ Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
@@ -1,29 +1,45 @@
 #include<stdio.h>
-int reverse( int arr[],int si,int ei);
-int reverse( int arr[],int si,int ei){
-    for(int i=si,j=ei;i<j;i++,j--){
-        int temp=arr[i];
-        arr[i] =arr[j];
-        arr[j] =temp;
+
+// Reverse function definition with void return type
+void reverse(int arr[], int si, int ei) {
+    for(int i = si, j = ei; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    
 }
-int main(){
-    int a,k;
-    scanf("%d",&a);
-    scanf("%d",&k);
+
+int main() {
+    int a, k;
+    
+    // Input number of elements and the value of k
+    scanf("%d", &a);
+    scanf("%d", &k);
+    
     int arr[a];
     
-    for(int i=0;i<a;i++){
-        scanf("%d",&arr[i]);
+    // Input the elements of the array
+    for (int i = 0; i < a; i++) {
+        scanf("%d", &arr[i]);
     }
-    k=k%a;
-    reverse(arr, 0,a-1);
-    reverse(arr, 0,k-1);
-    reverse(arr, k,a-1);
-    for(int i=0;i<a;i++){
-        printf("%d",arr[i]);
+    
+    // Adjust k to handle cases where k > a
+    k = k % a;
+    
+    // Reverse the entire array
+    reverse(arr, 0, a - 1);
+    
+    // Reverse the first part
+    reverse(arr, 0, k - 1);
+    
+    // Reverse the second part
+    reverse(arr, k, a - 1);
+    
+    // Print the rotated array with a space between each element
+    for (int i = 0; i < a; i++) {
+        printf("%d ", arr[i]);  // Added space between elements for readability
     }
+    printf("\n");  // Print a newline after the output
+    
     return 0;
-
 }
