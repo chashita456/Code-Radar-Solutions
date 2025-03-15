6ca4185c294b96c4e40510a97c949553995beb8c
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-03-15 12:11:31 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int prime(int a);
-int prime(int a){
+int prime(int n,int a);
+int prime(int n, int a){
     for(int i=n;i<=a;i++){
         prime =1;
         for(int j=n;j*j<=i;j++){
