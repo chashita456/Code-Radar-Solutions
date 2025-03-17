db459fcbadc331ee9788bc5d7377a3037599a70f
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-03-17 16:58:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 int fib(int a);
 int fib(int a){
-    if(a==1){
+    if(a == 1){
         return 0;
     }
-    if(a==2){
+    if(a == 2){
         return 1;
     }
-    int fibN =fib(a-1)+fib(a-2);
+    int fibN = fib(a - 1) + fib(a - 2); // Corrected recursive call
     return fibN;
-
 }
+
 int main(){
     int a;
-    scanf("%d",&a);
-    if(a<=0){
-    printf("Invalid input");
-    }
-    else{
-    printf("%d ",fib(a));
+    printf("Enter the position of the Fibonacci number: "); // Prompt for input
+    scanf("%d", &a);
+    if(a <= 0){
+        printf("Invalid input\n");
+    } else {
+        printf("Fibonacci number at position %d is %d\n", a, fib(a));
     }
     return 0;
 }
+
