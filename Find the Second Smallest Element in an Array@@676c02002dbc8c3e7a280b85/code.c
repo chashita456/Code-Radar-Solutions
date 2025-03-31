bad483f37a3adb0a0fb3238196923d9817e3a564
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-01 00:00:43 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -3,29 +3,22 @@
 int main(){
     int a;
     scanf("%d",&a);
-    int arr[a];
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
-    int min =INT_MAX;
-    int smin =INT_MAX;
-    int found =0;
+    int min = INT_MAX;
+    int smin = INT_MAX;
     for(int i=0;i<a;i++){
         if(arr[i]<min){
             smin = min;
             min = arr[i];
         }
-        else if(smin>arr[i] && min!=arr[i]){
-            smin = arr[i];
-            found =1;
+        else if((arr[i]<smin) && min!=arr[i] ){
+            smin =arr[i];
         }
-        
+
     }
-    if(found==1){
-        printf("%d",smin);
-    }
-    else{
-        printf("-1");
-    }
+    printf("%d",arr[i]);
     return 0;
+
 }
