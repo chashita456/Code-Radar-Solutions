0f31e49dc36b5ae77067b81e019a012ead65686c
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-08 18:06:00 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,13 +1,34 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    scanf("%d",&a);
-    if(a|1){
-        printf("Set");
+#include <stdio.h>
+
+int main() {
+    int num;
+    int zeros = 0, ones = 0;
+
+    printf("Enter a binary number (only 0s and 1s): ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        int digit = num % 10;  // Get last digit
+
+        if (digit == 0) {
+            zeros++;
+        } else if (digit == 1) {
+            ones++;
+        } else {
+            printf("Invalid input! Only 0 and 1 are allowed.\n");
+            return 1;
+        }
+
+        num = num / 10;  // Remove last digit
     }
-    else{
-        printf("Not set");
+
+    if (zeros > ones) {
+        printf("Most Common Bit (MCB): 0\n");
+    } else if (ones > zeros) {
+        printf("Most Common Bit (MCB): 1\n");
+    } else {
+        printf("Equal number of 0s and 1s.\n");
     }
+
     return 0;
 }
