4fb29e391b87a125f90e7417014d6fda1f2bbdc5
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-08 11:31:19 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -4,8 +4,8 @@ int main(){
 
     char s[100];
     char s1[100];
-    fgets(s,sizeofs,stdin);
-    fgets(s1,sizeofs1,stdin);
+    fgets(s,sizeof(s),stdin);
+    fgets(s1,sizeof(s1),stdin);
     
     strcat(s,s1);
     printf("%s",s);
