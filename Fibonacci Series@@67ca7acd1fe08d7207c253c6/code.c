89a60218be27177a892adf5930415a048cdab8c1
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-09 17:50:25 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
-int fib(int a);
-int fib(int a){
-    if(a == 1){
-        return 0;
+void fibonacciSeries(int n){
+    int t1=0,t2=1,nextTerm;
+    for(int i=1;i<=n;i++){
+        printf("%d ",t1);
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
     }
-    if(a == 2){
-        return 1;
-    }
-    int fibN = fib(a - 1) + fib(a - 2); // Corrected recursive call
-    return fibN;
+    printf("\n");
 }
-
-int main(){
-    int a;
-    
-    scanf("%d", &a);
-    if(a <= 0){
-        printf("Invalid input\n");
-    } else {
-        printf("%d", fib(a));
-    }
-    return 0;
-}
-
