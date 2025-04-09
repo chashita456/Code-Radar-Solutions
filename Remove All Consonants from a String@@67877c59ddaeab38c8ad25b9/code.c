a64dc3c852a7cdd5aa55d61671e10740ec9389bc
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-09 16:41:43 +0530

    Solution to Remove All Consonants from a String

--- Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
+++ Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
@@ -3,13 +3,17 @@
 int main(){
     char str[100];
     fgets(str,sizeof(str),stdin);
+    int flag =0;
     for(int i=0;str[i]!=0;i++){
         if(str[i]=='a'||str[i]=='e'||str[i]=='0'||str[i]=='u'||str[i]=='A'|| str[i]=='E'|| str[i]=='O'|| str[i]=='U'){
-            Printf("%s",str);
+           flag =1;
         }
-        else{
-            printf("");
-        }
+    }
+    if(flag==1){
+        printf("%s",str);
+    }
+    else{
+        printf("");
     }
     return 0;
 }
