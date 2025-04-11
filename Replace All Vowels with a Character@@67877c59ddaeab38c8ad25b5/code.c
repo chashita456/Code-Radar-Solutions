dd541f3ddaa4f3cb0909eb59e4802857e1d83fe6
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-11 11:12:17 +0530

    Solution to Replace All Vowels with a Character

--- Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
+++ Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
@@ -1,46 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-int isVowel(char c) {
-    c = tolower(c);
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-}
-
-int main() {
-    char str[100];
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char  str[100];
     char d;
-    int vowelIndex[100], count = 0;
-
-    // Input the character to replace vowels
-    scanf("%c", &d);
-    getchar(); // consume newline
-
-    // Input the string
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0'; // remove trailing newline
-
-    // Find indexes of all vowels
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (isVowel(str[i])) {
-            vowelIndex[count++] = i;
-        }
+    scanf("%c",&d);
+    
+    scanf("%s",str);
+    
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
+            str[i]=d;
+        }   
     }
-
-    // Reverse the vowels using the given character
-    for (int i = 0; i < count / 2; i++) {
-        // swap the vowels
-        char temp = str[vowelIndex[i]];
-        str[vowelIndex[i]] = str[vowelIndex[count - i - 1]];
-        str[vowelIndex[count - i - 1]] = temp;
-    }
-
-    // Replace all vowels with the character 'd'
-    for (int i = 0; i < count; i++) {
-        str[vowelIndex[i]] = d;
-    }
-
-    // Output result
-    printf("%s\n", str);
+    printf("%s",str);
     return 0;
+
 }
